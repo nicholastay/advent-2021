746a40a28ef3349b053315c6143d41f77332dbcf
Nicholas Tay <nick@windblume.net>
2021-12-04 01:49:51 +1100

    day02, part02 done!

--- day02/day02.rs
+++ day02/day02.rs
@@ -1,6 +1,49 @@
 use std::fs;
 use std::env;
 
+fn part1(data: &str, horiz: &mut i32, depth: &mut i32) {
+    for line in data.lines() {
+	let mut tokens = line.split(" ");
+	
+	let direction = tokens.next().unwrap();
+	let val = tokens.next().unwrap().parse::<i32>().unwrap();
+
+	match direction {
+	    "forward" => *horiz += val,
+	    "down" => *depth += val,
+	    "up" => *depth -= val,
+	    _ => {
+		println!("unreachable, panic");
+	        std::process::exit(1);
+	    },
+	}
+    }
+}
+
+fn part2(data: &str, horiz: &mut i32, depth: &mut i32) {
+    let mut aim: i32 = 0;
+
+    for line in data.lines() {
+	let mut tokens = line.split(" ");
+	
+	let direction = tokens.next().unwrap();
+	let val = tokens.next().unwrap().parse::<i32>().unwrap();
+
+	match direction {
+	    "forward" => {
+		*horiz += val;
+		*depth += aim * val;
+	    },
+	    "down" => aim += val,
+	    "up" => aim -= val,
+	    _ => {
+		println!("unreachable, panic");
+	        std::process::exit(1);
+	    },
+	}
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -15,23 +58,16 @@ fn main() {
     let mut horiz: i32 = 0;
     let mut depth: i32 = 0;
 
-    for line in data.lines() {
-	let mut tokens = line.split(" ");
-	
-	let direction = tokens.next().unwrap();
-	let val = tokens.next().unwrap().parse::<i32>().unwrap();
+    part1(&data, &mut horiz, &mut depth);
+    println!("Part 1");
+    println!("Horizontal position: {}, Depth: {}", horiz, depth);
+    println!("Multiplied: {}", horiz * depth);
+    println!("");
 
-	match direction {
-	    "forward" => horiz += val,
-	    "down" => depth += val,
-	    "up" => depth -= val,
-	    _ => {
-		println!("unreachable, panic");
-	        std::process::exit(1);
-	    },
-	}
-    }
-
+    horiz = 0;
+    depth = 0;
+    part2(&data, &mut horiz, &mut depth);
+    println!("Part 2");
     println!("Horizontal position: {}, Depth: {}", horiz, depth);
     println!("Multiplied: {}", horiz * depth);
 }
