90e7862acdcd150a3034b968b55193e426c3ef2f
Nicholas Tay <nick@windblume.net>
2021-12-12 19:06:51 +1100

    day09, part02 done
    The time it takes for this problem to be solved leaves much to be
    desired. My (ab)usage of vectors sucks, I should really think of a
    better way to tackle this question.

--- day09/day09.rs
+++ day09/day09.rs
@@ -1,6 +1,80 @@
 use std::env;
 use std::fs;
 
+type Map = Vec<Vec<u32>>;
+type Point = (usize, usize);
+
+// Returns lowest positions
+fn part1(map: &Map) -> Vec<Point> {
+    let mut low_points: Vec<Point> = Vec::new();
+    for (i, row) in map.iter().enumerate() {
+        for (j, height) in row.iter().enumerate() {
+            let adjacents = adjacent_points(map, (i, j));
+            if adjacents.iter().all(|&(r, c)| height < &map[r][c]) {
+                low_points.push((i, j));
+            }
+        }
+    }
+
+    low_points
+}
+
+fn adjacent_points(map: &Map, (r, c): Point) -> Vec<Point> {
+    let h = map.len();
+    let w = map[0].len();
+
+    let mut adjacents: Vec<Point> = Vec::new();
+    if r > 0 {
+        adjacents.push((r - 1, c));
+    }
+    if r < h - 1 {
+        adjacents.push((r + 1, c));
+    }
+    if c > 0 {
+        adjacents.push((r, c - 1));
+    }
+    if c < w - 1 {
+        adjacents.push((r, c + 1));
+    }
+
+    adjacents
+}
+
+// Returns 3 largest basin sizes multiplied
+fn part2(map: &Map) -> u32 {
+    // println!("{}", part2_basin_size(map, (0, 9), &mut Vec::new()));
+
+    let mut visited = Vec::new();
+    let mut sizes = (0..map.len())
+        .flat_map(|i| {
+            (0..map[0].len())
+                .map(|j| part2_basin_size(map, (i, j), &mut visited))
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<u32>>();
+    sizes.sort_unstable();
+
+    sizes.into_iter().rev().take(3).product::<u32>()
+}
+
+// There probably is a more elegant way than a visited vec...
+// Although, it lets us share the vector between executions, to avoid double counting
+// Each square is uniquely allocated to a single basin.
+fn part2_basin_size(map: &Map, point: Point, visited: &mut Vec<Point>) -> u32 {
+    let (r, c) = point;
+    if map[r][c] == 9 || visited.contains(&point) {
+        return 0;
+    }
+
+    // println!("counting {:?}", point);
+    visited.push(point);
+
+    1 + adjacent_points(map, point)
+        .iter()
+        .map(|p| part2_basin_size(map, *p, visited))
+        .sum::<u32>()
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -21,31 +95,13 @@ fn main() {
         })
         .collect::<Vec<Vec<u32>>>();
 
-    let h = map.len();
-    let w = map[0].len();
+    let low_points = part1(&map);
+    println!("Part 1 low points: {:?}", low_points);
+    println!(
+        "Part 1 risk level sum: {}",
+        low_points.iter().map(|&(r, c)| 1 + map[r][c]).sum::<u32>(),
+    );
 
-    let mut score: u32 = 0;
-    for (i, row) in map.iter().enumerate() {
-        for (j, height) in row.iter().enumerate() {
-            let mut adjacents: Vec<u32> = Vec::new();
-            if i > 0 {
-                adjacents.push(map[i - 1][j]);
-            }
-            if i < h - 1 {
-                adjacents.push(map[i + 1][j]);
-            }
-            if j > 0 {
-                adjacents.push(map[i][j - 1]);
-            }
-            if j < w - 1 {
-                adjacents.push(map[i][j + 1]);
-            }
-
-            if adjacents.iter().all(|a| height < a) {
-                score += 1 + height;
-            }
-        }
-    }
-
-    println!("Part 1 risk levels sum: {}", score);
+    println!("");
+    println!("Part 2 largest basin sizes multiplied: {}", part2(&map));
 }
