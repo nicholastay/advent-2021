8f3cf3520c349c9149f680c8a4350b0d17eaf9a5
Nicholas Tay <nick@windblume.net>
2021-12-22 15:33:10 +1100

    day18 part1 done
    Really should get better at doing these types of questions - takes a
    little bit for me to problem solve the right data structures to do this
    effectively (which is good that I'm doing it in Rust, if I was doing it
    in Python I 100% would've cheated with the ease of random allocations).
    
    Pretty happy with the parsing though - doing functional at uni seems to
    have helped this.

---
+++ day18/day18.rs
@@ -0,0 +1,148 @@
+use std::env;
+use std::fs;
+use std::str::Chars;
+use std::iter::Peekable;
+
+#[derive(Debug, Copy, Clone)]
+struct NestVal {
+    val: u32,
+    depth: u32,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("{} <input.txt>", &args[0]);
+        std::process::exit(1);
+    }
+
+    let file = fs::read(&args[1]).expect("Could not read input file given.");
+    let data = String::from_utf8_lossy(&file);
+
+    let pairs = data
+        .lines()
+        .map(parse_pair);
+    //println!("{:#?}", pairs.collect::<Vec<Vec<NestVal>>>());
+    let pairs_sum = pairs
+        .reduce(pairs_reduce_add)
+        .unwrap();
+    //println!("{:#?}", pairs_sum);
+
+    println!("Magnitude: {}", pairs_magnitude(pairs_sum));
+}
+
+fn parse_pair(pair: &str) -> Vec<NestVal> {
+    let mut v = Vec::new();
+    parse_pair_aux(&mut pair.chars().peekable(), 0, &mut v);
+    v
+}
+
+fn parse_expect(chars: &mut Peekable<Chars>, c: char) {
+    if chars.next().unwrap() != c {
+        unreachable!();
+    }
+}
+
+fn parse_pair_aux(chars: &mut Peekable<Chars>, depth: u32, v: &mut Vec<NestVal>) {
+    parse_expect(chars, '[');
+    parse_pairval(chars, depth, v);
+    parse_expect(chars, ',');
+    parse_pairval(chars, depth, v);
+    parse_expect(chars, ']');
+}
+
+fn parse_pairval(chars: &mut Peekable<Chars>, depth: u32, v: &mut Vec<NestVal>) {
+    if *chars.peek().unwrap() == '[' {
+        parse_pair_aux(chars, depth + 1, v);
+    } else {
+        v.push(
+            NestVal {
+                val: chars.next()
+                          .unwrap()
+                          .to_digit(10)
+                          .unwrap(),
+                depth,
+            }
+        );
+    }
+}
+
+fn pairs_reduce_add(p1: Vec<NestVal>, p2: Vec<NestVal>) -> Vec<NestVal> {
+    // Basic add the 2 first
+    let mut v = p1
+        .iter()
+        .map(|nv| NestVal { val: nv.val, depth: nv.depth + 1 })
+        .chain(
+            p2
+                .iter()
+                .map(|nv| NestVal { val: nv.val, depth: nv.depth + 1 })
+        )
+        .collect::<Vec<NestVal>>();
+
+    // Then perform explosions, splits, etc.
+    let mut actioned = true;
+    while actioned {
+        actioned = false;
+
+        // Always explode first if possible
+        for i in 0..v.len() {
+            if v[i].depth == 4 {
+                // Explode: 'i' will be left of the pair
+                // Assert that assumption + that the other pair exists
+                assert!(v[i].depth == v[i+1].depth);
+                if i > 0 {
+                    v[i-1].val += v[i].val;
+                }
+                if i < v.len() - 2 {
+                    v[i+2].val += v[i+1].val
+                }
+                v[i].val = 0;
+                v[i].depth -= 1;
+                v.remove(i+1);
+                actioned = true;
+                break;
+            }
+        }
+
+        if actioned {
+            continue;
+        }
+
+        // Then do splits
+        for i in 0..v.len() {
+            if v[i].val > 9 {
+                let rem = v[i].val % 2;
+                v[i].val /= 2;
+                v[i].depth += 1;
+                v.insert(i+1, NestVal { val: v[i].val + rem, depth: v[i].depth });
+                actioned = true;
+                break;
+            }
+        }
+    }
+
+    v
+}
+
+fn pairs_magnitude(v: Vec<NestVal>) -> u32 {
+    // There should be a more sound way to do this?
+    let mut vv = v.clone();
+    while vv.len() > 1 {
+        for i in 0..vv.len()-1 {
+            if vv[i].depth == vv[i+1].depth {
+                vv[i].val *= 3;
+                vv[i].val += 2 * vv[i+1].val;
+                vv.remove(i + 1);
+                if vv[i].depth == 0 {
+                    assert!(vv.len() == 1);
+                    return vv[0].val;
+                } else {
+                    vv[i].depth -= 1;
+                }
+                break;
+            }
+        }
+    }
+    unreachable!();
+}
